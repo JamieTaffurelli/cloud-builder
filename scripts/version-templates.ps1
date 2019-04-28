0b4f49fb31ad19253106793443262d361db78158
jamie taffurelli <jltaffurelli@outlook.com>
2019-04-28 21:29:37 +0100

    removed mandatory parameters

--- scripts/version-templates.ps1
+++ scripts/version-templates.ps1
@@ -6,14 +6,13 @@ param
     [String]
     $SearchFolder = "${env:SYSTEM_DEFAULTWORKINGDIRECTORY}\templates",
 
-    [Parameter(Mandatory = $true)]
+    [Parameter()]
     [ValidateScript( { $_ | Test-Path -PathType "Container" -IsValid } )]
     [String]
     $OutputFolder = $env:BUILD_ARTIFACTSTAGINGDIRECTORY
 )
     
 Import-Module "$(Split-Path -Path $PSScriptRoot)\module\azurebuild.psd1" -Force
-Write-Host "b: $OutputFolder"
-Write-Host "c: $env:BUILD_ARTIFACTSTAGINGDIRECTORY"
+
 Copy-AzBuildTemplateFilesWithVersion -SearchFolder $SearchFolder -OutputFolder $OutputFolder
 
