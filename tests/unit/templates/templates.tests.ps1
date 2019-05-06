b8bac5063b21057d8d4eb8a59d4fb3ffd42cb2f9
jamie taffurelli <jltaffurelli@outlook.com>
2019-05-06 11:02:48 +0100

    updated function and tests

--- tests/unit/templates/templates.tests.ps1
+++ tests/unit/templates/templates.tests.ps1
@@ -23,6 +23,11 @@ Describe "Template Validation" {
 
                 $arm.contentVersion -as [version] | should not be $null 
             }
+
+            It "${armTemplatePath} has a resource block" {
+
+                $arm.resources | should not be $null
+            }
         }
     }
 }
