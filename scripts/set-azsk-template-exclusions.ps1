0bc8fb8a3938724067ff96d45243945c58960211
jamie taffurelli <jltaffurelli@outlook.com>
2019-06-10 22:06:48 +0100

    refactored exclusions script

--- scripts/set-azsk-template-exclusions.ps1
+++ scripts/set-azsk-template-exclusions.ps1
@@ -7,33 +7,70 @@ param
     $SearchFolder = "${env:SYSTEM_DEFAULTWORKINGDIRECTORY}\templates",
 
     [Parameter()]
-    [String[]]
-    $InclusionFolders = @(
-        "Microsoft.Web\sites", 
-        "Microsoft.Cdn\profiles", 
-        "Microsoft.DocumentDb\databaseAccounts", 
-        "Microsoft.DataLakeStore\accounts", 
-        "Microsoft.Cache\Redis", 
-        "Microsoft.Sql\servers", 
-        "Microsoft.Storage\storageAccounts", 
-        "Microsoft.Network\trafficmanagerprofiles", 
-        "Microsoft.ServiceFabric\clusters", 
-        "Microsoft.ContainerService\ManagedClusters",
-        "Microsoft.Logic\workflows",
-        "Microsoft.ContainerRegistry\registries",
-        "Microsoft.KeyVault\vaults",
-        "Microsoft.Network\virtualNetworks",
-        "Microsoft.Search\searchServices",
-        "Microsoft.EventHub\namespaces",
-        "Microsoft.ContainerInstance\containerGroups"
-    ),
-
-    [Parameter()]
     [Switch]
     $AsCsv
 )
 
-$inclusionTemplates = @()
+$inclusionTemplates = @(
+    "Microsoft.Web\sites", 
+    "Microsoft.Cdn\profiles", 
+    "Microsoft.DocumentDb\databaseAccounts", 
+    "Microsoft.DataLakeStore\accounts", 
+    "Microsoft.Cache\Redis", 
+    "Microsoft.Sql\servers", 
+    "Microsoft.Storage\storageAccounts", 
+    "Microsoft.Network\trafficmanagerprofiles", 
+    "Microsoft.ServiceFabric\clusters", 
+    "Microsoft.ContainerService\ManagedClusters",
+    "Microsoft.Logic\workflows",
+    "Microsoft.ContainerRegistry\registries",
+    "Microsoft.KeyVault\vaults",
+    "Microsoft.Network\virtualNetworks",
+    "Microsoft.Search\searchServices",
+    "Microsoft.EventHub\namespaces",
+    "Microsoft.ContainerInstance\containerGroups"
+)
+
+$templateFilePaths = (Get-ChildItem -Path $SearchFolder -Recurse -File).FullName
+$excludedTemplateFileNames = @()
+
+foreach($templateFilePath in $templateFilePaths)
+{
+    $json = Get-Content -Path $templateFilePath | ConvertFrom-Json
+
+    if($inclusionTemplates -notcontains $json.resources.type)
+    {
+        Write-Verbose "${templateFilePath} will be excluded from AzSK ARM template scanning"
+        $excludedTemplateFileNames += Split-Path -Path $templateFilePath -Leaf
+    }
+}
+
+if($AsCsv)
+{
+    $csvExcludedTemplateFileNames = ($excludedTemplateFileNames -join ',')
+    Write-Host "##vso[task.setvariable variable=ExclusionTemplates]${csvExcludedTemplateFileNames}"
+
+    return $csvExcludedTemplateFileNames
+}
+else
+{
+    return $excludedTemplateFileNames
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 foreach($InclusionFolder in $InclusionFolders)
 {
