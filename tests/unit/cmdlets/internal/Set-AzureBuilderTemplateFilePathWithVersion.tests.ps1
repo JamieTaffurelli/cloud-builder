33ff36af734e0eceb5457ea5264e49d4001a461a
JamieTaffurelli <jltaffurelli@outlook.com>
2020-05-22 12:30:37 +0100

    corrected tests

--- tests/unit/cmdlets/internal/Set-AzureBuilderTemplateFilePathWithVersion.tests.ps1
+++ tests/unit/cmdlets/internal/Set-AzureBuilderTemplateFilePathWithVersion.tests.ps1
@@ -4,20 +4,20 @@ $internalCmdletDirectory = $PSScriptRoot -Replace [Regex]::Escape("tests\unit"),
 
 Describe "$(Split-Path -Path $PSCommandPath -Leaf)" {
 
-    New-Item -Path "TestDrive:\" -Name "WebServer.ps1" -ItemType "File"
+    New-Item -Path "TestDrive:\" -Name "template.json" -ItemType "File"
 
     Context "Parameter Validation" {
 
         It "Errors if Path does not exist" {
 
-            { "TestDrive:\file.ps1" | Set-AzureBuilderConfigurationFilePathWithVersion -ErrorAction Stop } | should throw 'Cannot validate argument on parameter 'Path'. The " ($PSItem | Test-Path -PathType "Leaf") -and ([System.IO.Path]::GetExtension($PSItem) -eq "ps1") " validation script for the argument with value "TestDrive:\file.json" did not return a result of True. Determine why the validation script failed, and then try the command again.'
+            { "TestDrive:\file.json" | Set-AzureBuilderTemplateFilePathWithVersion -ErrorAction Stop } | should throw 'Cannot validate argument on parameter 'Path'. The " ($PSItem | Test-Path -PathType "Leaf") -and ([System.IO.Path]::GetExtension($PSItem) -eq "json") " validation script for the argument with value "TestDrive:\file.json" did not return a result of True. Determine why the validation script failed, and then try the command again.'
         }
 
         It "Errors if Path is not a JSON file" {
 
             New-Item -Path "TestDrive:\" -Name "template.txt" -ItemType "File"
 
-            { "TestDrive:\template.txt" | Set-AzureBuilderConfigurationFilePathWithVersion -ErrorAction Stop } | should throw 'Cannot validate argument on parameter 'Path'. The " ($PSItem | Test-Path -PathType "Leaf") -and ([System.IO.Path]::GetExtension($PSItem) -eq "ps1") " validation script for the argument with value "TestDrive:\file.json" did not return a result of True. Determine why the validation script failed, and then try the command again.'
+            { "TestDrive:\template.txt" | Set-AzureBuilderTemplateFilePathWithVersion -ErrorAction Stop } | should throw 'Cannot validate argument on parameter 'Path'. The " ($PSItem | Test-Path -PathType "Leaf") -and ([System.IO.Path]::GetExtension($PSItem) -eq "json") " validation script for the argument with value "TestDrive:\file.json" did not return a result of True. Determine why the validation script failed, and then try the command again.'
         }
     }
 
@@ -25,29 +25,25 @@ Describe "$(Split-Path -Path $PSCommandPath -Leaf)" {
 
         It "Errors if contentVersion is not valid" {
 
-            Mock -CommandName Get-Content -MockWith { 'configuration WebServer' } -Verifiable
+            Mock -CommandName Get-Content -MockWith {'{}'} -Verifiable
 
-            { "TestDrive:\WebServer.ps1" | Set-AzureBuilderConfigurationFilePathWithVersion -ErrorAction Stop } | should throw "WebServer does not end with a 3 digit verion (without periods '.')"
+            { "TestDrive:\template.json" | Set-AzureBuilderTemplateFilePathWithVersion -ErrorAction Stop } | should throw "TestDrive:\template.json does not have a valid content version ''"
 
-            Mock -CommandName Get-Content -MockWith { 'configuration WebServer1x2' } -Verifiable
+            Mock -CommandName Get-Content -MockWith {'{ "contentVersion": "invalid"}'} -Verifiable
 
-            { "TestDrive:\WebServer.ps1" | Set-AzureBuilderConfigurationFilePathWithVersion -ErrorAction Stop } | should throw "WebServer1x2 does not end with a 3 digit verion (without periods '.')"
+            { "TestDrive:\template.json" | Set-AzureBuilderTemplateFilePathWithVersion -ErrorAction Stop } | should throw "TestDrive:\template.json does not have a valid content version 'invalid'"
 
-            Mock -CommandName Get-Content -MockWith { 'configuration WebServer1234' } -Verifiable
-
-            { "TestDrive:\WebServer.ps1" | Set-AzureBuilderConfigurationFilePathWithVersion -ErrorAction Stop } | should throw "WebServer1234 does not end with a 3 digit verion (without periods '.')"
-
-            Assert-MockCalled -CommandName Get-Content -Times 3 -Scope It -Exactly -ParameterFilter { $Path -eq "TestDrive:\WebServer.ps1" }
-            Assert-MockCalled -CommandName Get-Content -Times 3 -Scope It -Exactly
+            Assert-MockCalled -CommandName Get-Content -Times 2 -Scope It -Exactly -ParameterFilter { $Path -eq "TestDrive:\template.json" }
+            Assert-MockCalled -CommandName Get-Content -Times 2 -Scope It -Exactly
         }
 
         It "Returns versioned file path" {
 
-            Mock -CommandName Get-Content -MockWith { 'configuration WebServer123' } -Verifiable
+            Mock -CommandName Get-Content -MockWith {'{ "contentVersion": "1.0.0.0"}'} -Verifiable
 
-            "TestDrive:\WebServer.ps1" | Set-AzureBuilderConfigurationFilePathWithVersion -ErrorAction Stop | should be "TestDrive:\WebServer123.ps1"
+            "TestDrive:\template.json" | Set-AzureBuilderTemplateFilePathWithVersion -ErrorAction Stop | should be "TestDrive:\template.1.0.0.0.json"
 
-            Assert-MockCalled -CommandName Get-Content -Times 1 -Scope It -Exactly -ParameterFilter { $Path -eq "TestDrive:\WebServer.ps1" }
+            Assert-MockCalled -CommandName Get-Content -Times 1 -Scope It -Exactly -ParameterFilter { $Path -eq "TestDrive:\template.json" }
             Assert-MockCalled -CommandName Get-Content -Times 1 -Scope It -Exactly
         }
     }
