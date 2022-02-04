d1cfba82052bd1e6e72b4188465bd35df8660de8
Jamie Taffurelli <jltaffurelli@outlook.com>
2022-02-04 00:09:05 +0000

    added config storage account

---
+++ examples/config-rg/config-rg.tfvars
@@ -0,0 +1,7 @@
+resource_group_name                            = "config"
+location                                       = "eastus"
+log_analytics_workspace_name                   = "logging-law"
+log_analytics_workspace_resource_group_name    = "logging"
+storage_account_name                           = "configsajdn"
+storage_account_resource_group_name            = "logging"
+tags                                           = {}
---
+++ examples/config-rg/variables.tf
@@ -0,0 +1,34 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Resource Group name of the Storage Account"
+}
+
+variable "location" {
+  type        = string
+  description = "Location of the Resource Group"
+}
+
+variable "log_analytics_workspace_name" {
+  type        = string
+  description = "Name of Log Analytics Workspace to send diagnostics"
+}
+
+variable "log_analytics_workspace_resource_group_name" {
+  type        = string
+  description = "Resource Group of Log Analytics Workspace to send diagnostics"
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "Name of Storage Account to create"
+}
+
+variable "storage_account_resource_group_name" {
+  type        = string
+  description = "Resource Group of Storage Account to create"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags of the Virtual Machine"
+}
